96e557663f5b1fbef1e1c7723b2b3ce0d16bc445
Luizfgr23 <147094631+Luizfgr23@users.noreply.github.com>
2024-03-01 09:16:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data_processor import DataProcessor
 
 def main():
-    processor=DataProcessor('ODS','DW','bi','paçoquinha','172.22.1.45','5432') # Após a criação da URL de conexão e suas instâncias,
+    processor=DataProcessor('ODS','DW','----','----','----','5432') # Após a criação da URL de conexão e suas instâncias,
     processor.execute()                                                        # passaremos os valores para os respectivos objetos
 
 if __name__ == "__main__":
