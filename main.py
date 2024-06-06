2b056bbb1736417ef67771a729f88d1069142291
Luizfgr23 <147094631+Luizfgr23@users.noreply.github.com>
2024-06-06 12:26:51 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from data_processor import DataProcessor
-
-def main():
-    processor=DataProcessor('ODS','DW','----','----','----','5432') # Após a criação da URL de conexão e suas instâncias,
-    processor.execute()                                                        # passaremos os valores para os respectivos objetos
-
-if __name__ == "__main__":
-    main() # Executa a funcao se ela existir
